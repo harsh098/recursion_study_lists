c07e97d040fc08f009cce6e60e4bf1b6a92f6c6e
Harsh Mishra <hmisraji07@gmail.com>
2024-01-06 10:02:56 +0530

    Optimise permutations.py
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

--- leetcode_solutions/permutations.py
+++ leetcode_solutions/permutations.py
@@ -2,23 +2,45 @@ from typing import List, Set
 
 
 # Approach 1 Generate All Pairs
+# class Solution:
+#     def __perm(self, i: int, ds: List[int], src: List[int], chosen: Set[int], res: List[int]):
+#         if i==len(src):
+#             res.append(ds[:])
+#             return
+        
+#         for j in range(len(src)):
+#             if j in chosen:
+#                 continue
+#             chosen.add(j)
+#             ds.append(src[j])
+#             self.__perm(i+1, ds, src, chosen, res)
+#             ds.pop()
+#             chosen.remove(j)
+    
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res =[]
+#         chosen = set()
+#         self.__perm(0, [], nums, chosen, res)
+#         return res
+
+# Approach 2 (Optimisation 1:- Remove HashSet) Generate All Pairs
 class Solution:
-    def __perm(self, i: int, ds: List[int], src: List[int], chosen: Set[int], res: List[int]):
+    def __perm(self, i: int, ds: List[int], src: List[int], chosen: List[bool], res: List[int]):
         if i==len(src):
             res.append(ds[:])
             return
         
         for j in range(len(src)):
-            if j in chosen:
+            if chosen[j]:
                 continue
-            chosen.add(j)
+            chosen[j]=True
             ds.append(src[j])
             self.__perm(i+1, ds, src, chosen, res)
             ds.pop()
-            chosen.remove(j)
+            chosen[j]=False
     
     def permute(self, nums: List[int]) -> List[List[int]]:
         res =[]
-        chosen = set()
+        chosen = [False]*len(nums)
         self.__perm(0, [], nums, chosen, res)
         return res
