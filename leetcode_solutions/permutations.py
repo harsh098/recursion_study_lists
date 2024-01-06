1313169247a5fe85d95ec1ffd5da90882e0e2c4d
Harsh Mishra <hmisraji07@gmail.com>
2024-01-06 10:14:55 +0530

    Add Jugglung And Placement To permutations.py
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

--- leetcode_solutions/permutations.py
+++ leetcode_solutions/permutations.py
@@ -24,23 +24,40 @@ from typing import List, Set
 #         return res
 
 # Approach 2 (Optimisation 1:- Remove HashSet) Generate All Pairs
+# class Solution:
+#     def __perm(self, i: int, ds: List[int], src: List[int], chosen: List[bool], res: List[int]):
+#         if i==len(src):
+#             res.append(ds[:])
+#             return
+        
+#         for j in range(len(src)):
+#             if chosen[j]:
+#                 continue
+#             chosen[j]=True
+#             ds.append(src[j])
+#             self.__perm(i+1, ds, src, chosen, res)
+#             ds.pop()
+#             chosen[j]=False
+    
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res =[]
+#         chosen = [False]*len(nums)
+#         self.__perm(0, [], nums, chosen, res)
+#         return res
+
+# Approach 3 (Optimal: Optimised Space) Use juggling and placement
 class Solution:
-    def __perm(self, i: int, ds: List[int], src: List[int], chosen: List[bool], res: List[int]):
+    def __perm(self, i: int,  src: List[int], res: List[int]):
         if i==len(src):
-            res.append(ds[:])
+            res.append(src[:])
             return
         
-        for j in range(len(src)):
-            if chosen[j]:
-                continue
-            chosen[j]=True
-            ds.append(src[j])
-            self.__perm(i+1, ds, src, chosen, res)
-            ds.pop()
-            chosen[j]=False
+        for j in range(i, len(src)):
+            src[i], src[j] = src[j], src[i]
+            self.__perm(i+1, src, res)
+            src[i], src[j] = src[j], src[i]
     
     def permute(self, nums: List[int]) -> List[List[int]]:
         res =[]
-        chosen = [False]*len(nums)
-        self.__perm(0, [], nums, chosen, res)
+        self.__perm(0, nums, res)
         return res
